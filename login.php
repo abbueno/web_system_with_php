a9b7135dbfe101fb47a0a109b6318683137de803
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-02-20 19:10:09 -0300

    Initial_Commit
    Initial Commit

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    // pegar valores do formulário
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+    // conexão com bd
+
+
+    // verificar email e senha
+
+?>
