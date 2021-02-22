2b51f55d0b0a6345c7dc9b5150965146ff4c6a31
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-02-22 13:34:07 -0300

    atualizacao
    atualização

--- login.php
+++ login.php
@@ -4,11 +4,11 @@ $email = $_POST["email"];
 $senha = $_POST["senha"];
 
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;restaurante_bd", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
